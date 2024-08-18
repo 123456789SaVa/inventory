d9578637fbe18d03f48f2a4f6be7c3ca2f1bbbd8
sava <savakitaychuk@gmail.com>
2024-08-18 13:32:25 +0300

    Initial commit
    Signed-off-by: sava <savakitaychuk@gmail.com>

---
+++ app/src/main/java/com/Sava/inventory/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.Sava.inventory
+/**/
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Column {
+
+
+                Row {
+                    Text("1")
+                    Text("2")
+                    Text("3")
+                }
+                Row {
+                    Text("4")
+                    Text("5")
+                    Text("6")
+                }
+
+                Text("Привет")
+                Text("я")
+                Text("профессионал")
+                Text("в")
+                Text("программировании")
+                Button(onClick = { }) {
+                    Text(text = "hello")
+                }
+            }
+
+
+        }
+    }
+}
+
