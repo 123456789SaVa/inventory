740ec10610051d3675200821e6eb47cd818b1cb2
sava <savakitaychuk@gmail.com>
2024-11-10 11:53:32 +0200

    сделал инвентарь

---
+++ app/src/main/java/com/Sava/inventory/DataClass.kt
@@ -0,0 +1,12 @@
+package com.Sava.inventory
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "items")
+data class Item(
+    @PrimaryKey(autoGenerate = true) val id: Long,
+    val name: String,
+    val price: Long,
+    val quantity: Long
+)
--- app/src/main/java/com/Sava/inventory/inventory.kt
+++ app/src/main/java/com/Sava/inventory/inventory.kt
@@ -1,76 +1,176 @@
-package com.Sava.inventory
-
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableLongStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
+import com.Sava.inventory.Item
+import com.Sava.inventory.ItemViewModel
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun InventoryApp() {
-    Row {
-        Scaffold(topBar = {
-            TopAppBar(
-                title = { Text(text = "Привет") },
-                colors = topAppBarColors(containerColor = Color.Green)
+fun InventoryScreen(navController: NavHostController, viewModel: ItemViewModel) {
+    val allItems by viewModel.allItems.collectAsStateWithLifecycle(listOf())
+    Column(
+        Modifier
+            .padding(16.dp)
+            .fillMaxSize()
+    ) {
+        Row {
+            Text(
+                "ITEM", Modifier
+                    .weight(2F)
+                    .align(Alignment.CenterVertically)
+                    .padding(start = 8.dp)
             )
-        })
-        { padding ->
-            val navController = rememberNavController()
-            NavHost(
-                navController = navController,
-                startDestination = INVENTORY_SCREEN,
-                modifier = Modifier.padding(padding)
-            ) {
-                composable(INVENTORY_SCREEN) { InventoryScreen(navController) }
-                composable(ADD_ITEM_SCREEN) { AddItemScreen(navController) }
-                composable(EDIT_ITEM_SCREEN) { EditItemScreen(navController) }
-                composable(ITEM_DETAILS_SCREEN) { ItemDetailsScreen(navController) }
+            Text(
+                "PRICE", Modifier
+                    .weight(1F)
+                    .align(Alignment.CenterVertically), textAlign = TextAlign.Center
+            )
+            Text(
+                "QUANTITY IN STOCK", Modifier
+                    .weight(1F)
+                    .align(Alignment.CenterVertically), textAlign = TextAlign.Center
+            )
+        }
+        Spacer(Modifier.height(8.dp))
+        HorizontalDivider()
+        LazyColumn {
+            items(allItems) { item ->
+                ItemView(item, { itemData ->
+                    viewModel.selectedItem = itemData
+                    navController.navigate(ITEM_DETAILS_SCREEN)
+                })
             }
         }
-    }//test
-}
-@Composable
-fun AddItemScreen(navController: NavHostController) {
+    }
 }
 
 @Composable
-fun EditItemScreen(navController: NavHostController) {
-
+fun ItemView(item: Item, onClick: (Item) -> Unit) {
+    Row(
+        Modifier
+            .height(48.dp)
+            .clickable { onClick(item) }
+    ) {
+        Text(
+            item.name, Modifier
+                .weight(2F)
+                .align(Alignment.CenterVertically)
+                .padding(start = 8.dp)
+        )
+        Text(
+            item.price.toString(), Modifier
+                .weight(1F)
+                .align(Alignment.CenterVertically), textAlign = TextAlign.Center
+        )
+        Text(
+            item.quantity.toString(), Modifier
+                .weight(1F)
+                .align(Alignment.CenterVertically), textAlign = TextAlign.Center
+        )
+    }
 }
 
 @Composable
-fun ItemDetailsScreen(navController: NavHostController) {
-
+fun AddEditItemScreen(navController: NavHostController, viewModel: ItemViewModel) {
+    var name by rememberSaveable { mutableStateOf(viewModel.selectedItem?.name ?: "") }
+    var price by rememberSaveable {
+        mutableStateOf(viewModel.selectedItem?.price?.toString() ?: "")
+    }
+    var quantity by rememberSaveable {
+        mutableStateOf(viewModel.selectedItem?.quantity?.toString() ?: "")
+    }
+    Column(modifier = Modifier.padding(16.dp)) {
+        TextField(name, { name = it }, modifier = Modifier.fillMaxWidth(), label = {
+            Text("Item Name")
+        })
+        Spacer(Modifier.height(16.dp))
+        TextField(
+            value = price,
+            onValueChange = { price = it },
+            modifier = Modifier.fillMaxWidth(),
+            keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
+            label = { Text("Item Price") }
+        )
+        Spacer(Modifier.height(16.dp))
+        TextField(
+            value = quantity,
+            onValueChange = { quantity = it },
+            keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
+            modifier = Modifier.fillMaxWidth(),
+            label = { Text("Quantity in Stock") }
+        )
+        Spacer(Modifier.height(32.dp))
+        Button({
+            val newItem = if (viewModel.selectedItem != null)
+                viewModel.selectedItem!!.copy(
+                    name = name, price = price.toLongOrNull() ?: 0,
+                    quantity = quantity.toLongOrNull() ?: 0
+                ) else Item(
+                id = 0, name = name, price = price.toLongOrNull() ?: 0,
+                quantity = quantity.toLongOrNull() ?: 0
+            )
+            viewModel.insertItem(newItem)
+            if (viewModel.selectedItem != null) viewModel.selectedItem = newItem
+            navController.popBackStack()
+        }, modifier = Modifier.fillMaxWidth()) { Text("Save") }
+    }
 }
-
 
 @Composable
-fun InventoryScreen(navController: NavHostController) {
+fun ItemDetailsScreen(navController: NavHostController, viewModel:ItemViewModel) {
+    var quantity by rememberSaveable { mutableLongStateOf(viewModel.selectedItem?.quantity ?: 0) }
+    LaunchedEffect(Unit) { quantity = viewModel.selectedItem?.quantity ?: 0 }
+    Column(Modifier.padding(16.dp)) {
+        Text(viewModel.selectedItem?.name ?: "", fontSize = 16.sp)
+        Spacer(Modifier.height(16.dp))
+        Text("$ " + viewModel.selectedItem?.price, fontSize = 16.sp)
+        Spacer(Modifier.height(16.dp))
+        Text("Quantity in Stock: $quantity", fontSize = 16.sp)
+        Spacer(Modifier.height(16.dp))
+        Button({
+            if (quantity > 0) {
+                val newItem = viewModel.selectedItem!!.copy(quantity = quantity - 1)
+                viewModel.insertItem(newItem)
+                quantity -= 1
+                viewModel.selectedItem = newItem
+            }
+        }, modifier = Modifier.fillMaxWidth()) { Text("Sell") }
+        OutlinedButton({
+            viewModel.selectedItem?.id?.let {
+                viewModel.deleteItemById(it)
+                navController.navigateUp()
+            }
+        }, modifier = Modifier.fillMaxWidth()) { Text("Delete") }
+    }
 }
 
-
-
-val INVENTORY_SCREEN = "Inventory"
-val ADD_ITEM_SCREEN = "Add Item"
-val EDIT_ITEM_SCREEN = "Edit Item"
-val ITEM_DETAILS_SCREEN = "Item Details"
-///tests
+const val INVENTORY_SCREEN = "Inventory"
+const val ADD_EDIT_ITEM_SCREEN = "Add/Edit Item"
+const val ITEM_DETAILS_SCREEN = "Item Details"
